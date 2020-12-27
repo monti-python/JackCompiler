d699d7354dfefbd0d68efaf8c39e774146ee1239
Angel Montana <a.montana.red@gmail.com>
2020-12-27 01:43:10 +0100

    Fix rules. Make more tests pass

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -6,18 +6,29 @@ import main.java.binding.TokenAdapter;
 import main.java.binding.TokenList;
 import main.java.exceptions.JackCompilerException;
 import main.java.model.Token;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 import javax.xml.bind.JAXBException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stax.StAXResult;
 import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -45,9 +56,9 @@ public class ParserTest {
 
 
     @Test
-    public void testParser() throws JackCompilerException, ParserConfigurationException, TransformerException {
-        //Path path = Paths.get("src/test/resources/TestParser.jck");
-        System.out.println(path.toString());
+    public void testParser() throws JackCompilerException, ParserConfigurationException, TransformerException, IOException, SAXException, XMLStreamException {
+        System.out.println("Testing file: " + path.toString());
+        Path ref_path = Paths.get(path.toString().replaceFirst("\\.jack$", ".xml"));
         List<Token> res_tokens = Tokenizer.tokenize(path);
         Parser parser = new Parser(res_tokens);
         Element element = parser.comp("class");
@@ -57,10 +68,17 @@ public class ParserTest {
         transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        transformer.setOutputProperty(OutputKeys.METHOD, "html");
+
         StringWriter writer = new StringWriter();
         transformer.transform(new DOMSource(element), new StreamResult(writer));
-        String output = writer.getBuffer().toString();
 
-        System.out.println(output);
+        String raw_out = writer.getBuffer().toString().replaceAll("\\s", "");
+        String raw_ref = new String(Files.readAllBytes(ref_path)).replaceAll("\\s", "");
+
+        //System.out.println(raw_out);
+        //System.out.println(raw_ref);
+        Assert.assertEquals(raw_out, raw_ref);
+
     }
 }
